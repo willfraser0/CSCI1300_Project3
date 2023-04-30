d7fa2b412a9367445ac90341ab89153cc3e6a085
willfraser0 <130501041+willfraser0@users.noreply.github.com>
2023-04-30 16:24:19 -0600

    Update Party.h

--- main/Party.h
+++ main/Party.h
@@ -10,12 +10,14 @@ using namespace std;
 class Party {
     private:
 
+        int rooms_cleared;
         int gold_coins;
         int ingredients;
 
         int cookware[3]; // {ceramic pot (P), cauldron (C), }frying pan (F),
         int treasures[5]; // {Silver ring (R), Ruby necklace (N), Emerald bracelet (B), Diamond circlet (C), Gem-encrusted goblet (G)}
-
+        int weapons[5] = {0};
+        bool armor[5] = {0};
         vector<Player> party_members;
 
     public:
@@ -24,6 +26,11 @@ class Party {
         void addMember(string member_name);
         void removeMember(int member_id);
         Player Member(int member_id);
+    
+        string getLastMemberName();
+
+        void addRoomCleared();
+        int getRoomsCleared();
 
         void setGold(int input_gold);
         void addGold(int input_gold);
@@ -36,8 +43,21 @@ class Party {
         void displayInventory();
         void cook();
 
-        void addCookware(char input_cookware);
+        void addCookware(char input_cookware, int cookware_amount);
         void addTreasure(char input_treasure);
+        int getTreasurePrice();
+
+        void getWeapons();
+        void addWeapons(char weapon_type, int amount);
+
+        void getArmor();
+        void addArmor(int armor_amount);
+    
+        void getWeaponStrength();
+    
+        int getWeaponDifference();
+    
+        
 
 
 };
